7f48bb6ef245804250eca5a329ab15ccee5ff336
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-10-24 14:53:16 +0200

    Ideen als Kommentare hinzugefügt.

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ export function aufgabe08(args) {
   return args.toLowerCase()
 }
 
+/* Klassisch imperative Weise zum Programmieren.
+ * Kann recht einfach verstanden werden. Eine leere Liste wird
+ * vor der Schlaufe definiert und in der Schleife gefüllt.
+ *
+ * Scheint einfach zu verstehen zu sein, setzt aber voraus das Schleifen verstanden wurden.
+ * Das konnte ich bis jetzt noch nicht so feststellen. Das durchlaufen einer Schlaufe,
+ * in der sich die Werte mit jedem Durchgang verändern, scheint ein Komplexitätsschritt zu
+ * sein, den viele SuS nicht machen können.
+ */
 export function aufgabe00(args) {
   let input = args.split("")
   let res = []
@@ -28,6 +37,11 @@ export function aufgabe00(args) {
   return res.join("")
 }
 
+/* Funktionales Programmieren, wie es oft in Javascript anzutreffen ist.
+ * Hat den Vorteil das wir anonyme Funktionen verwenden können, wird aber mit der Syntax schwieriger.
+ * Wir könnten Arrow-Functions wählen, damit könnte man die Syntax leicht unterscheiden. Hat dennoch
+ * das Problem der Verschachtelung und wird somit Komlexer.
+ */
 export function aufgabe01(args) {
   const res = each(args, function (c) {
     if (c === " ") {
@@ -47,6 +61,17 @@ function filterAufgabe02(c) {
   }
 }
 
+/* Funktionales Programmieren mit einer externen Funktion.
+ * Pro:
+ * + So modular wie möglich.
+ * + Teile können wieder verwendet werden.
+ * + Teile können auch einfach auf einem kleinen Level getestet werden.
+ * + Filter sind einfach zu lesen.
+ * Contra:
+ * - Zusätlche Funktionen die nur an einer Stelle verwendet werden.
+ * - Komlexität wird verteilt.
+ * - Aufgabe selber wird zu unnötigem Boilerplate-Code.
+ */
 export function aufgabe02(args) {
   return each(args, filterAufgabe02)
 }
