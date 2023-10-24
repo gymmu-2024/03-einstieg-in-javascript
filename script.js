9daab39556c7b4551e17380c40ffde6e2bc4f8ae
Cedric Geissmann <cedric.geissmann@gmail.com>
2023-10-24 12:53:44 +0200

    Verschiedene implementierungen für Aufgabe 1.

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-export function aufgabe01(args) {
-  let input = args
-  return `Hello ${input}`
-}
-
 export function sayHello() {}
 
 export function changeBackground() {}
@@ -18,4 +13,40 @@ export function writeText() {}
 export function aufgabe08(args) {
   let input = args
   return args.toLowerCase()
+}
+
+export function aufgabe00(args) {
+  let input = args.split("")
+  let res = []
+  for (const e of input) {
+    if (e === " ") {
+      res.push("_")
+    } else {
+      res.push(e)
+    }
+  }
+  return res.join("")
+}
+
+export function aufgabe01(args) {
+  const res = each(args, function (c) {
+    if (c === " ") {
+      return "_"
+    } else {
+      return c
+    }
+  })
+  return res
+}
+
+function filterAufgabe02(c) {
+  if (c === " ") {
+    return "_"
+  } else {
+    return c
+  }
+}
+
+export function aufgabe02(args) {
+  return each(args, filterAufgabe02)
 }
