852816a09dbae4f1c3f2852b73ad21f77b57c335
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-10-15 14:21:19 +0000

    Add comments.

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 import { linkupExerciseHandler } from "./utils"
 
+/**
+ * Wir erstellen hier eine Funktion für die Aufgabe 1. Funktionen sind praktisch
+ * um den Code innerhalb, vom rest zu trennen. Dadurch können wir Variablen mit
+ * gleichem Namen auch in anderen Funktionen verwenden, ohne das deren Wert
+ * überschrieben wird.
+ * Funktionen können auch verwendet werden, um an anderer Stelle wieder
+ * verwendet zu werden. Wir machen das in diesem Projekt eigentlich nicht, aber
+ * wenn Sie des Resultat von einer Funktion nochmals brauchen, können Sie das
+ * wie folgt machen:
+ *    `const withoutE = aufgabe01("Hier ist ein Text mit einigen e's")`
+ * Damit wird der Code in aufgabe01 ausgeführt, der sollte alle e's entfernen,
+ * und das Resultate wird in der Variable `withoutE` gespeichert, und kann dann
+ * weiter verwendet werden.
+ */
 export function aufgabe01(args) {
+  // Wir speichern hier den Wert von args in der Variable `input` ab. Damit soll für uns klarer werden, womit wir arbeiten.
   const input = args
+
+  // Wir erzeugen hier eine leere Liste, in der wir das Resultat Stück für Stück anhängen.
   const result = []
 
+  // Mit dieser Schlaufe nummerieren wir jedes Zeichen in `input` durch. Das
+  // machen wir um jedes Zeichen einzeln anzuschauen.
   for (let i = 0; i < input.length; i++) {
+    // Hier speichern wir das Zeichen an der Stelle `i` in der Variable
+    // `currentElement`, damit es explizit ist womit wir arbeiten, aber auch
+    // damit wir nicht so viele Klammern schreiben müssen.
     const currentElement = input[i]
+
+    // Hier wird das aktuelle Zeichen ans Ende der Resultat-Liste angehängt.
     result.push(currentElement)
   }
+
+  // Hier geben wir das Resultat zurück, und machen einen Text daraus.
   return result.join("")
 }
 
