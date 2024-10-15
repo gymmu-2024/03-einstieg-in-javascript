2636abf38d406a61ad4785fedb0eb578e18e718c
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-10-15 14:06:05 +0000

    Fix missing export for tests.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 import { linkupExerciseHandler } from "./utils"
 
-function aufgabe01(args) {
+export function aufgabe01(args) {
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-
     result.push(currentElement)
   }
   return result.join("")
